78755cb44de5c94230fb862410b7d87fe81fb64e
FabianMonjaraz <d.fabian.monjaraz@gmail.com>
2022-04-05 14:50:10 -0500

    Manejando creacion de bucket

--- crear_carpetas.sh
+++ crear_carpetas.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-crea_bucket(){
-gsutil mb -p crp-dev-iac-testing -c standard -l us-central1 -b on gs://crp-dev-iac-testing-bkt07
+init(){
+  PROYECTO=$1
+  BKT=$2
+  if [[ $PROYECTO != $(gcloud info | grep project | awk '{print $2}' |tr -d "[]") ]]; then
+    gcloud config set project crp-dev-iac-testing
+  fi
+  if [[ -z "$(gsutil ls | grep $BKT)" ]]; then
+    gsutil mb -p crp-dev-iac-testing -c standard -l us -b on $BKT
+    echo "> Creando bucket $BKT"
+  else
+    echo "> El bucket $BKT ya existe"
+  fi
 }
 
 info_team() {
@@ -15,9 +25,10 @@ msg() {
   echo "> $1"
 }
 
-crea_bucket
-
+PROYECTO=crp-dev-iac-testing
 BKT="gs://crp-dev-iac-testing-bkt07/"
+init $PROYECTO $BKT
+
 msg "Creacion de carpetas"
 mkdir DATA
 for I in $(seq 100); do
